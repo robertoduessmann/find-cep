c18dc8dc02092ed1cac7ff785c2f324dd3d20c40
Roberto Duessmann <roberto.duessmann@gmail.com>
2018-03-13 23:15:07 -0300

    Extract button to pure component

---
+++ src/Button.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = ({children}) => (
+    <StyledButton type='submit'>{ children }</StyledButton>    
+)
+
+const StyledButton = styled.button`
+display: inline-block;
+border-radius: 3px;
+padding: 1rem 0;
+margin: 0.5rem 1rem;
+width: 11rem;
+background: #8899ef;
+color: white;
+border: 2px solid white;
+font-size: 20px;
+`
+
+export default Button
